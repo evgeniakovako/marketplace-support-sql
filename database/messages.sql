a21e92f69e9271a1f91c54236cea4ba7d3d64d3e
Evgenia <ainutlaatuinen1@gmail.com>
2026-07-12 16:54:24 +0700

    Delete database/messages.sql

--- database/messages.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE messages (
-    message_id INT PRIMARY KEY,
-    sender_id INT,
-    receiver_id INT,
-    ad_id INT,
-    message_text TEXT,
-    sent_at DATETIME,
-    is_read BOOLEAN
-);
